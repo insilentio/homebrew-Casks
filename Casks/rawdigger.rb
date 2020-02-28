cfd5a808b28e2660211b62669378862a729839c2
insilentio <30929451+insilentio@users.noreply.github.com>
2020-02-28 09:49:46 +0100

    Update rawdigger.rb
    fix sha again

--- Casks/rawdigger.rb
+++ Casks/rawdigger.rb
@@ -1,6 +1,6 @@
 cask 'rawdigger' do
   version '1.4.0.670'
-  sha256 'c787ee7029ec428c2403665c14e1c190d52de02280dea494eeb36733fa7e79'
+  sha256 'c787ee7029ec428c2403665c1a4e1c190d52de02280dea494eeb367336fa7e79'
 
   url "https://delivery.rawdigger.com/data/RawDigger-#{version}.dmg"
   appcast 'https://www.rawdiggerr.com/download'
