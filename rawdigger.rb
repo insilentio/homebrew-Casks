202badc5c6299089ef845f5c98937a4a010697e6
insilentio <30929451+insilentio@users.noreply.github.com>
2020-02-28 09:37:05 +0100

    Update rawdigger.rb
    fix version

--- rawdigger.rb
+++ rawdigger.rb
@@ -2,7 +2,7 @@ cask 'rawdigger' do
   version '1.4.0.670'
   sha256 'C787EE7029EC428C2403665C1A4E1C190D52DE02280DEA494EEB367336FA7E79'
 
-  url "https://delivery.rawdigger.com/data/RawDigger-1.4.0.670.dmg"
+  url "https://delivery.rawdigger.com/data/RawDigger-#{version}.dmg"
   appcast 'https://www.rawdiggerr.com/download'
   name 'RawDigger'
   homepage 'https://www.rawdigger.com'
