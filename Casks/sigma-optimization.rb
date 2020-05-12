13cf269f8192e4279749ff2d95daafd048911804
insilentio <30929451+insilentio@users.noreply.github.com>
2020-05-12 21:26:39 +0200

    Update sigma-optimization.rb

--- Casks/sigma-optimization.rb
+++ Casks/sigma-optimization.rb
@@ -1,8 +1,8 @@
 cask 'sigma-optimization' do
-  version '151'
+  version '1.5.1'
   sha256 'c9487bb2ca35506e364b8de29d785de09b9400757d912305e068941d5e2613aa'
 
-  url "https://www.sigma-global.com/common/download/lenses/sigma-optimization-pro/data/SIGMA_Optimization_Pro#{version}.dmg"
+  url "https://www.sigma-global.com/common/download/lenses/sigma-optimization-pro/data/SIGMA_Optimization_Pro151.dmg"
   appcast 'https://www.sigma-global.com/en/download/lenses'
   name 'SigmaOptimizationPro'
   homepage 'https://www.sigma-global.com/en'
