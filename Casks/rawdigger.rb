1fe97473f96010f6711e89a1b30699a24ce804d9
Daniel Baumgartner <30929451+insilentio@users.noreply.github.com>
2022-02-11 17:55:03 +0100

    Update rawdigger.rb
    update to v 1.4.5.727

--- Casks/rawdigger.rb
+++ Casks/rawdigger.rb
@@ -1,6 +1,6 @@
 cask 'rawdigger' do
-  version '1.4.2.691'
-  sha256 '846484f6cbf50c81ed726275b1ad4161010098bcaeeedcd3e70470efac1658f0'
+  version '1.4.5.727'
+  sha256 '9e96d7eac9618a28fbf0912e909a5c298315aab487f6bd996548e69e7ad8a771'
 
   url "https://delivery.rawdigger.com/data/RawDigger-#{version}.dmg"
   appcast 'https://www.rawdigger.com/download'
