21105d2cdd2e34a040ee8f04bbc487a1799fad20
insilentio <insilentio@protonmail.ch>
2023-11-03 19:23:27 +0100

    replace appcast with livecheck

--- Casks/familienbande.rb
+++ Casks/familienbande.rb
@@ -3,7 +3,7 @@ cask 'familienbande' do
   sha256 '5abdd87ef1a3e6f4d8ab6729e8a3b2a093ea47fdcc7d00d2100de55ce38ad69b'
 
   url "https://www.familienbande-genealogie.de/download/FB-Intel64.dmg"
-  appcast 'https://www.familienbande-genealogie.de/download'
+  livecheck 'https://www.familienbande-genealogie.de/download'
   name 'Familienbande'
   homepage 'https://www.familienbande-genealogie.de'
 
--- Casks/rawdigger.rb
+++ Casks/rawdigger.rb
@@ -3,7 +3,7 @@ cask 'rawdigger' do
   sha256 '9e96d7eac9618a28fbf0912e909a5c298315aab487f6bd996548e69e7ad8a771'
 
   url "https://delivery.rawdigger.com/data/RawDigger-#{version}.dmg"
-  appcast 'https://www.rawdigger.com/download'
+  livecheck 'https://www.rawdigger.com/download'
   name 'RawDigger'
   homepage 'https://www.rawdigger.com'
 
